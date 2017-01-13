5290dd5320d23a2f783b59aaa5354a006be0e997
khillion <kenzo-hugo.hillion1@pasteur.fr>
2017-01-13 11:31:36 +0100

    also catch ERROR and not only FAILS in CI tests

--- run_test.sh
+++ run_test.sh
@@ -4,7 +4,7 @@ LOGS=$(mktemp)
 nosetests --with-coverage --cover-package=pycwl 2> $LOGS
 cat $LOGS
 grep pycwl $LOGS | awk '{ print "TOTAL: "$4; }'
-TEST=$(grep FAILED $LOGS)
+TEST=$(egrep -e 'FAILED|ERROR' $LOGS)
 rm $LOGS
 if [[ ! -z $TEST ]]; then echo "Tests failed." && exit 1; fi
 exit 0
