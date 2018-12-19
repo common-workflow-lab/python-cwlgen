95fdd831ecc15cb59cff36bf793e1921bdfc09c4
Michael Franklin <michael@illusional.net>
2018-12-20 09:53:52 +1100

    Corrects spelling mistakes on CommandLineBinding

--- cwlgen/commandlinebinding.py
+++ cwlgen/commandlinebinding.py
@@ -1,16 +1,20 @@
 class CommandLineBinding(object):
-    '''
-    Describes how the handle an input or an argument.
-    '''
+    """
+    The binding behavior when building the command line depends on the data type of the value.
+    If there is a mismatch between the type described by the input schema and the effective value,
+    such as resulting from an expression evaluation, an implementation must use the data type of the effective value.
+
+    Documentation: https://www.commonwl.org/v1.0/CommandLineTool.html#CommandLineBinding
+    """
 
     def __init__(self, load_contents=False, position=None, prefix=None, separate=True,
                  item_separator=None, value_from=None, shell_quote=True):
-        '''
+        """
         :param load_contents: Read up to the fist 64 KiB of text from the file and
                               place it in the "contents" field of the file object
         :type load_contents: BOOLEAN
         :param position: The sorting key
-        :type positio: INT
+        :type position: INT
         :param prefix: Command line prefix to add before the value
         :type prefix: STRING
         :param separate:
@@ -21,7 +25,7 @@ class CommandLineBinding(object):
         :type value_from: STRING
         :param shell_quote: Value is quoted on the command line
         :type shell_quote: BOOLEAN
-        '''
+        """
         self.loadContents = load_contents
         self.position = position
         self.prefix = prefix
@@ -31,11 +35,11 @@ class CommandLineBinding(object):
         self.shellQuote = shell_quote
 
     def get_dict(self):
-        '''
+        """
         Transform the object to a [DICT] to write CWL.
 
-        :return: dictionnary of the object
+        :return: dictionary of the object
         :rtype: DICT
-        '''
+        """
         dict_binding = {k: v for k, v in vars(self).items() if v is not None}
         return dict_binding
