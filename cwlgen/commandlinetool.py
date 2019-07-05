e68526900b29ad98da4f17625865061c8b51c725
Michael Franklin <michael@illusional.net>
2019-07-05 10:31:13 +1000

    Adds better class auto-documentation structure

--- cwlgen/commandlinetool.py
+++ cwlgen/commandlinetool.py
@@ -17,12 +17,12 @@ _LOGGER = logging.getLogger(__name__)
 
 
 class CommandOutputBinding(Serializable):
-    '''
+    """
     Describes how to generate an output parameter based on the files produced.
-    '''
+    """
 
     def __init__(self, glob=None, load_contents=None, output_eval=None):
-        '''
+        """
         :param glob: Find corresponding file(s)
         :type glob: STRING
         :param load_contents: For each file matched, read up to the 1st 64 KiB of text and
@@ -30,22 +30,32 @@ class CommandOutputBinding(Serializable):
         :type load_contents: BOOLEAN
         :param output_eval: Evaluate an expression to generate the output value
         :type output_eval: STRING
-        '''
+        """
         self.glob = glob
         self.loadContents = load_contents
         self.outputEval = output_eval
 
 
 class CommandInputParameter(Parameter):
-    '''
+    """
     An input parameter for a :class:`cwlgen.CommandLineTool`.
-    '''
+    """
 
     parse_types = [("inputBinding", [CommandLineBinding])]
 
-    def __init__(self, param_id, label=None, secondary_files=None, param_format=None,
-                 streamable=None, doc=None, input_binding=None, default=None, param_type=None):
-        '''
+    def __init__(
+        self,
+        param_id,
+        label=None,
+        secondary_files=None,
+        param_format=None,
+        streamable=None,
+        doc=None,
+        input_binding=None,
+        default=None,
+        param_type=None,
+    ):
+        """
         :param param_id: unique identifier for this parameter
         :type param_id: STRING
         :param label: short, human-readable label
@@ -66,24 +76,40 @@ class CommandInputParameter(Parameter):
         :type default: STRING
         :param param_type: type of data assigned to the parameter corresponding to CWLType
         :type param_type: STRING
-        '''
-        Parameter.__init__(self, param_id=param_id, label=label,
-                           secondary_files=secondary_files, param_format=param_format,
-                           streamable=streamable, doc=doc, param_type=param_type)
+        """
+        Parameter.__init__(
+            self,
+            param_id=param_id,
+            label=label,
+            secondary_files=secondary_files,
+            param_format=param_format,
+            streamable=streamable,
+            doc=doc,
+            param_type=param_type,
+        )
         self.inputBinding = input_binding
         self.default = default
 
 
 class CommandOutputParameter(Parameter):
-    '''
+    """
     An output parameter for a :class:`cwlgen.CommandLineTool`.
-    '''
+    """
 
     parse_types = [("outputBinding", [CommandOutputBinding])]
 
-    def __init__(self, param_id, label=None, secondary_files=None, param_format=None,
-                 streamable=None, doc=None, output_binding=None, param_type=None):
-        '''
+    def __init__(
+        self,
+        param_id,
+        label=None,
+        secondary_files=None,
+        param_format=None,
+        streamable=None,
+        doc=None,
+        output_binding=None,
+        param_type=None,
+    ):
+        """
         :param param_id: unique identifier for this parameter
         :type param_id: STRING
         :param label: short, human-readable label
@@ -102,27 +128,48 @@ class CommandOutputParameter(Parameter):
         :type output_binding: :class:`cwlgen.CommandOutputBinding` object
         :param param_type: type of data assigned to the parameter corresponding to CWLType
         :type param_type: STRING
-        '''
-        Parameter.__init__(self, param_id, label, secondary_files, param_format, streamable,
-                           doc, param_type)
+        """
+        Parameter.__init__(
+            self,
+            param_id,
+            label,
+            secondary_files,
+            param_format,
+            streamable,
+            doc,
+            param_type,
+        )
         self.outputBinding = output_binding
 
 
 class CommandLineTool(Serializable):
-    '''
+    """
     Contain all informations to describe a CWL command line tool.
-    '''
+    """
 
-    __CLASS__ = 'CommandLineTool'
+    __CLASS__ = "CommandLineTool"
 
-    parse_types = [('inputs', [[CommandInputParameter]]), ("outputs", [[CommandOutputParameter]])]
+    parse_types = [
+        ("inputs", [[CommandInputParameter]]),
+        ("outputs", [[CommandOutputParameter]]),
+    ]
     ignore_fields_on_parse = ["namespaces", "class"]
 
-    def __init__(self, tool_id=None, base_command=None, label=None, doc=None,
-                 cwl_version=None, stdin=None, stderr=None, stdout=None, path=None):
-        '''
-        :param tool_id: unique identifier for this tool
-        :type tool_id: STRING
+    def __init__(
+        self,
+        tool_id=None,
+        base_command=None,
+        label=None,
+        doc=None,
+        cwl_version=None,
+        stdin=None,
+        stderr=None,
+        stdout=None,
+        path=None
+    ):
+        """
+        :param tool_id: Unique identifier for this tool
+        :type tool_id: str
         :param base_command: command line for the tool
         :type base_command: STRING | list[STRING]
         :param label: label of this tool
@@ -137,16 +184,19 @@ class CommandLineTool(Serializable):
         :type stderr: STRING
         :param stdout: capture stdout into the given file
         :type stdout: STRING
-
-        inputs (:class:`cwlgen.CommandInputParameter` objects),
+        Inputs (:class:`cwlgen.CommandInputParameter` objects),
         outputs (:class:`cwlgen.CommandOutputParameter` objects),
         arguments (:class:`cwlgen.CommandLineBinding` objects),
         hints (any | :class:`cwlgen.Requirement` objects)
         and requirements (:class:`cwlgen.Requirement` objects)
         are stored in lists which are initialized empty.
-        '''
+        """
         if cwl_version not in CWL_VERSIONS:
-            _LOGGER.warning("CWL version {} is not recognized as a valid version.".format(cwl_version))
+            _LOGGER.warning(
+                "CWL version {} is not recognized as a valid version.".format(
+                    cwl_version
+                )
+            )
             _LOGGER.warning("CWL version is set up to {}.".format(DEF_VERSION))
             cwl_version = DEF_VERSION
         self.cwlVersion = cwl_version
@@ -173,7 +223,7 @@ class CommandLineTool(Serializable):
 
         d = super(CommandLineTool, self).get_dict()
 
-        d['class'] = self.__CLASS__
+        d["class"] = self.__CLASS__
 
         if self.metadata:
             for key, value in self.metadata.__dict__.items():
@@ -181,11 +231,11 @@ class CommandLineTool(Serializable):
             # - Add Namespaces
             d[self.namespaces.name] = {}
             for k, v in self.namespaces.__dict__.items():
-                if '$' not in v:
+                if "$" not in v:
                     d[self.namespaces.name][k] = v
 
         if self.requirements:
-            d['requirements'] = {r.get_class(): r.get_dict() for r in self.requirements}
+            d["requirements"] = {r.get_class(): r.get_dict() for r in self.requirements}
         return d
 
     def export_string(self):
@@ -201,10 +251,10 @@ class CommandLineTool(Serializable):
 
         # Write CWL file in YAML
         if outfile is None:
-            six.print_(CWL_SHEBANG, "\n", sep='')
+            six.print_(CWL_SHEBANG, "\n", sep="")
             six.print_(rep)
         else:
-            out_write = open(outfile, 'w')
-            out_write.write(CWL_SHEBANG + '\n\n')
+            out_write = open(outfile, "w")
+            out_write.write(CWL_SHEBANG + "\n\n")
             out_write.write(rep)
             out_write.close()
---
+++ doc/source/workflowclasses.rst
@@ -0,0 +1,44 @@
+.. python-cwlgen - Python library for manipulation and generation of CWL tools.
+
+.. _classes:
+
+********************
+Workflow API classes
+********************
+
+.. _CWLGen - Workflow:
+
+
+CWL Workflow
+============
+
+Workflow
+""""""""
+
+.. autoclass:: cwlgen.workflow.Workflow
+    :members:
+    :private-members:
+    :special-members:
+    :exclude-members: __weakref__
+
+
+Inputs and Outputs
+==================
+
+InputParameter
+""""""""""""""
+
+.. autoclass:: cwlgen.workflow.InputParameter
+    :members:
+    :private-members:
+    :special-members:
+    :exclude-members: __weakref__
+
+WorkflowStep
+""""""""""""
+
+.. autoclass:: cwlgen.workflow.WorkflowStep
+    :members:
+    :private-members:
+    :special-members:
+    :exclude-members: __weakref__
